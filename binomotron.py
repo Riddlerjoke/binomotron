443a52ec881f3f078ac9bf04a75ab383ca1c7a44
riddlerjoke <58588665+Riddlerjoke@users.noreply.github.com>
2023-05-16 14:31:26 +0200

    Add files via upload

---
+++ binomotron.py
@@ -0,0 +1,86 @@
+import mysql.connector as sqlbdd
+import random
+from datetime import datetime
+from tkinter import *
+
+window = Tk()
+window.title("Générateur de binome")
+window.geometry("1080x720")
+window.iconbitmap()
+window.config(background="#3CCFFF")
+
+frame = Frame(window, bg="#3CCFFF")
+
+width = 512
+height = 512
+
+image = PhotoImage().zoom(20).subsample(32)
+canvas = Canvas(frame, width=width, height=height, bg="#3CCFFF", bd=0, highlightthickness=0)
+canvas.create_image(width/2, height/2, image=image)
+canvas.grid(row=0, column=0, sticky=W)
+
+right_frame = Frame(frame, bg=("#3CCFFF"))
+
+
+mybdd = sqlbdd.connect(
+    host="localhost",  
+    user="root",  
+    passwd="example",  
+    db="etudiant",
+    port="3307")  
+
+print("Connection réussie")
+cursor = mybdd.cursor()
+
+#Récuperer la table apprenants dans la base de donnée et les afficher :
+
+
+query = "SELECT id, prenom_eleve, nom_eleve FROM liste_etudiant"
+
+cursor.execute(query)
+liste_etudiant = []
+
+for identifiant, prenom, nom in cursor :
+    personne = [identifiant, prenom, nom]
+    liste_etudiant.append(personne)
+
+
+print("Voici la liste des etudiants :\n", liste_etudiant)
+
+#fonction qui permet d'executer la creation de groupe en fonction du nombre de 
+#personnes souhaité dans le groupe !
+
+
+nombre_personne_par_groupe = int(input("Combien de personnes par groupe ?"))
+groupes = []
+
+def groupe_eleve(liste):
+    global nombre_personne_par_groupe
+    global groupes
+    random.shuffle(liste)
+
+    while liste != []:
+        restant = len(liste)%nombre_personne_par_groupe
+        if restant == 0:
+            groupes.append(liste[0:nombre_personne_par_groupe])
+            del liste[0:nombre_personne_par_groupe]
+        else:
+            groupes.append(liste[0:restant])
+            del liste[0:restant]
+    return groupes
+
+groupe_eleve(liste_etudiant)
+
+# affichage des groupes en fonction de la demande de l'utilisateur
+
+print("Voici les groupes :")
+numero_de_groupe = 0
+for groupe in groupes:
+    numero_de_groupe += 1
+    affichage_groupes = ''.join(' - '.join([str(elem) for elem in groupe]))
+    affichage_groupes = affichage_groupes.replace('[','').replace(']','').replace("'",'')
+    print( f"Groupe {numero_de_groupe} = {affichage_groupes}")
+
+
+cursor.close()
+mybdd.close()
